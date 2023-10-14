56cd06a3f0021bb329d523084de07495d14b567d
Dima163163 <khlopovdima@gmail.com>
2023-10-14 18:24:54 +0400

    modal and acordeon fix

--- js/modules/accordeon.js
+++ js/modules/accordeon.js
@@ -2,11 +2,16 @@
 
 export const accordeonActive = (button, item) => {
   const btns = document.querySelectorAll(button);
-
+  const items = document.querySelectorAll(item);
   btns.forEach((btn, index) => {
-    btn.addEventListener('click', (e) => {
-      const target = e.target;
-      target.closest(item).classList.toggle('accordion__item_active');
+    btn.addEventListener('click', () => {
+      for (let i = 0; i < items.length; i++) {
+        if (index === i) {
+          items[i].classList.toggle('accordion__item_active');
+        } else {
+          items[i].classList.remove('accordion__item_active');
+        }
+      }
     });
   });
 };
--- js/modules/elementsPage.js
+++ js/modules/elementsPage.js
@@ -1,7 +1,9 @@
 const btnOpenModal = document.querySelector('.header__btn');
 const overlayModal = document.querySelector('.form-overlay');
+const sectionQuestion = document.querySelector('.question');
 
 export default {
   btnOpenModal,
   overlayModal,
+  sectionQuestion,
 };
